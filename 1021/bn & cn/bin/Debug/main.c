103894a9fb317d23dcae5216605d3a5d7fe5d26c
mdyasinahmed <yasinahmed7875@gmail.com>
2023-01-17 09:22:47 +0600

    Problem solving on beecrowd with C

---
+++ 1021/bn & cn/bin/Debug/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+    double amount;
+    int a, b, c, d, e, f, g, h, i, j, k, l;
+
+    scanf("%d", &n);
+    l = n * 100;
+
+
+    return 0;
+}
---
+++ beecrowd - average 1/bin/Debug/main (2).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double A, B;
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+    double C;
+    C = (A+B)/2;
+    printf("MEDIA = %.5lf\n", C);
+    return 0;
+}
