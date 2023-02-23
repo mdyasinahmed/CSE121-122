9863076b4ce0a93e6f30a1d57f23850a2b3f6973
mdyasinahmed <yasinahmed7875@gmail.com>
2023-02-23 22:38:00 +0600

    PositiveNumber Problem solved

--- 1060/PositiveNumbers.c
+++ 1060/PositiveNumbers.c
@@ -2,5 +2,19 @@
 
 int main()
 {
-    
+    int n=6, i, count=0;
+    double arr[n];
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%lf", &arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if (arr[i]>0)
+        {
+            count++;
+        }
+    }
+    printf("%d valores positivos\n", count);
 }
